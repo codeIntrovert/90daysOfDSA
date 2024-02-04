eea764f80af329150fa5bc8d4ad63252a1ff57ba
Hasan <9131411681hasan@gmail.com>
2024-02-04 17:42:33 +0530

    bulk folder maker script

---
+++ src/Extras/foldermaker.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM THIS CODE HAS BEEN WRITTEN BY CHATGPT, USED TO MAKE FOLDERS IN BULK
+REM DO NOT RUN UNLESS YOU KNOW WHAT YOU ARE DOING, FOR ADMIN USE ONLY.
+REM Set the number of days you want folders for INCRESE AS REQUIRED
+
+set /A "num_days=10" 
+
+REM Loop to create folders
+for /L %%i in (1,1,%num_days%) do (
+    set "day=%%i"
+    set "day=0!day!"
+    set "day=!day:~-2!"
+    mkdir "Day !day!"
+)
+
+echo Folders created successfully.
