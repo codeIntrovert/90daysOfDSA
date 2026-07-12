bb2d7a792df962e7199f80d1d40b13b4edbbcc4d
Hasan <46672438+codeIntrovert@users.noreply.github.com>
2026-07-12 06:35:43 +0530

    [Problem] PLUS ONE

---
+++ src/Day1.java
@@ -0,0 +1,23 @@
+/**
+ * TODAY WE LEARN THE BASICS HOW INPUTS WORKS IN DSA ETC ETC 
+ * https://leetcode.com/problems/plus-one/?envType=study-plan-v2&envId=programming-skills
+ */
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int count = digits.length;
+
+        for (int i = count - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newArray = new int[count + 1];
+        newArray[0] = 1; // Set first digit to 1
+        
+        return newArray; 
+    }
+}
